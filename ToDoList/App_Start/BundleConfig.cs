20c64c51dc3280340ca6a2fd001f9322d19e267e
mostafawaheed98 <waheedmostafa00@gmail.com>
2019-04-13 01:36:14 +0200

    Login & Register Front-end is finished

--- ToDoList/App_Start/BundleConfig.cs
+++ ToDoList/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace ToDoList
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.min.css",
                       "~/Content/site.css"));
         }
     }
